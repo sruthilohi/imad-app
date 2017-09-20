515c4fa048818263ed1dc6dd1b2caeceef31eb61
sruthi lohithakshan <sruthilohi@gmail.com>
2017-09-20 16:43:42 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-    var articleName = req.params.articleName;
+    var articleName=req.params.articleName;
   res.send(createtemplate(articles[articleName]));
 });
 

f5271a261abed4ff100b0d7e32d5c5a865db3cab
sruthi lohithakshan <sruthilohi@gmail.com>
2017-09-21 11:13:27 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element= document.getElementById('main-text');
 element.innerHTML='new value';
 // move the image
-var img =  document.getElementById('madi');
+var img = document.getElementById('madi');
 img.onclick= function(){
     img.style.marginleft='100px';
 };

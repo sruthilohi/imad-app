fa36af80e830e313f2c4ddd88b0a1ebcb54bf6d8
sruthi lohithakshan <sruthilohi@gmail.com>
2017-09-25 19:10:47 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var names=[];
-//
+//app.get('/submit_name/:name', function(req,res){
 app.get('/submit_name', function(req,res){
     //get the name from the request
     var name = req.query.name;

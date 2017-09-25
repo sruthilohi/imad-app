3df5d4088a5ed94c4aee09f55699902e6b620da8
sruthi lohithakshan <sruthilohi@gmail.com>
2017-09-25 12:59:35 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/* console.log('Loaded!');
 var element= document.getElementById('main-text');
 element.innerHTML='new value';
 // move the image
@@ -11,4 +11,11 @@ function moveRight(){
 img.onclick = function() {
     var interval= setInterval(moveRight, 50);
    
+};*/
+var button= document.getElementById('counter');
+var counter= counter+1;
+button.onclick = function(){
+    counter=counter=1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 };

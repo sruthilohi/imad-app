4180e2906fa90ee8c869a5d280cfcbe89db24e7a
sruthi lohithakshan <sruthilohi@gmail.com>
2017-10-09 11:36:08 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,6 +97,10 @@ var htmltemplate= `
 `;
 return htmltemplate;
 }
+app.get('/test-db',function(req,res){
+  //make a select request 
+  //return a response with the results
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });

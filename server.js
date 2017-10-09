9fea9a7b3ebaa6e8c88a3c3723625a8c2b63b6fd
sruthi lohithakshan <sruthilohi@gmail.com>
2017-10-09 12:24:53 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     database: 'sruthilohi',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:'DB_PASSWORD'
+    password: process.env.DB_PASSWORD
 };
 var app = express();
 app.use(morgan('combined'));
@@ -104,9 +104,17 @@ var htmltemplate= `
 `;
 return htmltemplate;
 }
+var pool= new pool(config);
 app.get('/test-db',function(req,res){
   //make a select request 
   //return a response with the results
+  pool.query('select *from test',function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }else {
+          res.send(JSON.stringify(result));
+      }
+  });
 });
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));

13152687f52d53f00c024b598f4316a1a27f223e
sruthi lohithakshan <sruthilohi@gmail.com>
2017-10-12 15:54:51 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.get('/submit_name', function(req,res){
 app.get('/articles/:articleName', function(req, res) {
     //articleName=articleone
     //articles[articleName]={}content object for article one
-    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err, result) {
+    pool.query("SELECT * FROM article WHERE title = $1 " , [req.params.articleName], function(err, result) {
       if(err){
           res.status(500).send(err.toString());
           }else

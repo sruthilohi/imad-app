388e52869993a35ece5009de125fe0f7e86df9da
sruthi lohithakshan <sruthilohi@gmail.com>
2017-11-14 17:33:10 +0400

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -37,7 +37,8 @@ function loadCommentForm () {
         request.open('POST', '/submit-comment/' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
         console.log(JSON.stringify({comment:comment }));
-        request.send(JSON.stringify({comment:comment}));  
+       //  request.send(JSON.stringify({title:title, heading:heading, content:content  })); 
+        request.send(JSON.stringify({comment:comment }));  
         submit.value = 'Submitting...';
         
     };

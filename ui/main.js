8887df5bb7a2df96d764b552092f5653df066678
sruthi lohithakshan <sruthilohi@gmail.com>
2017-11-14 15:46:59 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -126,8 +126,8 @@ function loadLoginForm(){
         // Make the request
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
+        //console.log(username);
+        //console.log(password);
         request.open('POST', '/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
         

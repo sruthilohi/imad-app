729e9bf981e46514f8591a1bde369cc47cdeb2c8
sruthi lohithakshan <sruthilohi@gmail.com>
2018-04-07 10:09:57 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -73,7 +73,7 @@ function loadLoginForm(){
           if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
-                  submit.value = 'Sucess!';
+                  submit.value = 'Success!';
               } else if (request.status === 403) {
                   submit.value = 'Invalid credentials. Try again?';
               } else if (request.status === 500) {
